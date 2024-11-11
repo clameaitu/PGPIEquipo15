a75d24699ce2466af6f8048f3f463a2d19529687
Daniel Flores <danflode@alum.us.es>
2024-11-11 14:05:33 +0100

    feat(cesta): cesta de compra en español

--- myshop/cart/cart.py
+++ myshop/cart/cart.py
@@ -20,7 +20,7 @@ class Cart(object):
         product_id = str(product.id)
         if product_id not in self.cart:
             self.cart[product_id] = {'quantity': 0,
-                                     'price': str(product.price)}
+                                     'price': str(product.precio)}
         if override_quantity:
             self.cart[product_id]['quantity'] = quantity
         else:
