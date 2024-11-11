24b4056b4d0d739681ebc790af9f109ea15596e5
Rafael Castillo Cebolla <rafaelcastillocebolla@gmail.com>
2024-11-11 11:14:45 +0100

    feat: set everything in Spanish
    He cambiado el idioma del sistema, que hace automáticamete que cambie la mayoría de las cosas del admin.
    Pero no era suficiente, porque los nombres de los atributos se usan directamente en pantalla, por lo que había que cambiarlos y ponerlos en español.
    La sección de internacionalización del libro es muy larga y creo que no vale la pena. Hace traducciones automáticas al código pero yo lo veo menos fiable y más tedioso, no sé.
    Las clases las he dejado como Product y Category, y los nombres de variables sin importancia los he dejado en inglés también. Lo único que he cambiado han sido los nombres de los atributos.
    
    Aparte, también he cambiado en frontend ya que estábamos los nombres de los títulos y eso al español.
    El nombre de la tienda que he puesto es provisional, poned el que queráis. Era para ver que se cambiaba.

--- myshop/shop/admin.py
+++ myshop/shop/admin.py
@@ -4,13 +4,13 @@ from .models import Category, Product
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug']
-    prepopulated_fields = {'slug': ('name',)}
+    list_display = ['nombre', 'slug']
+    prepopulated_fields = {'slug': ('nombre',)}
     
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug', 'price',
-                    'available', 'created', 'updated']
-    list_filter = ['available', 'created', 'updated']
-    list_editable = ['price', 'available']
-    prepopulated_fields = {'slug': ('name',)}
+    list_display = ['nombre', 'slug', 'precio',
+                    'disponible', 'creado', 'actualizado']
+    list_filter = ['disponible', 'creado', 'actualizado']
+    list_editable = ['precio', 'disponible']
+    prepopulated_fields = {'slug': ('nombre',)}
--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 
+
 def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
-    products = Product.objects.filter(available=True)
+    products = Product.objects.filter(disponible=True)
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
-        products = products.filter(category=category)
+        products = products.filter(categoria=category)
     return render(request,
                   'shop/product/list.html',
                   {'category': category,
@@ -18,7 +19,7 @@ def product_detail(request, id, slug):
     product = get_object_or_404(Product,
                                 id=id,
                                 slug=slug,
-                                available=True)
+                                disponible=True)
     return render(request,
                   'shop/product/detail.html',
                   {'product': product})
