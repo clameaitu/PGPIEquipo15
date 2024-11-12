0eabc41287d7ccb255c29d0f5087ab633cbb3b45
Rafael Castillo Cebolla <rafaelcastillocebolla@gmail.com>
2024-11-12 19:27:13 +0100

    style: linting changes to code

--- myshop/cart/views.py
+++ myshop/cart/views.py
@@ -3,6 +3,8 @@ from django.views.decorators.http import require_POST
 from shop.models import Product
 from .cart import Cart
 from .forms import CartAddProductForm
+
+
 @require_POST
 def cart_add(request, product_id):
     cart = Cart(request)
