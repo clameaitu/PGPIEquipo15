ada572899d9d0c716b8ff704c95c9a5f60bf49d1
Rafael Castillo Cebolla <rafaelcastillocebolla@gmail.com>
2024-11-15 18:39:02 +0100

    test: solve test orders after changes

--- myshop/orders/tests.py
+++ myshop/orders/tests.py
@@ -5,8 +5,8 @@ from orders.models import Order, OrderItem
 class OrderTests(TestCase):
     def setUp(self):
         self.category = Category.objects.create(
-            nombre="Electrónica",
-            slug="electronica"
+            nombre="Colonias",
+            slug="colonias"
         )
 
         self.product = Product.objects.create(
@@ -17,35 +17,36 @@ class OrderTests(TestCase):
         )
 
         self.order = Order.objects.create(
-            first_name="John",
-            last_name="Doe",
+            nombre="John",
+            apellidos="Doe",
             email="john@example.com",
-            address="123 Main St",
-            postal_code="12345",
-            city="Test City"
+            dirección="123 Main St",
+            código_postal="12345",
+            ciudad="Test City",
+            entrega_en_oficina_de_correos="False"
         )
 
     def test_create_order_item_succesful(self):
         order_item = OrderItem.objects.create(
             order=self.order,
-            product=self.product,
-            quantity=2,
-            price=self.product.precio
+            producto=self.product,
+            cantidad=2,
+            precio=self.product.precio
         )
         
-        self.assertEqual(order_item.product, self.product)
-        self.assertEqual(order_item.quantity, 2)
+        self.assertEqual(order_item.producto, self.product)
+        self.assertEqual(order_item.cantidad, 2)
         self.assertEqual(order_item.order, self.order)
-        self.assertEqual(order_item.price, self.product.precio)
+        self.assertEqual(order_item.precio, self.product.precio)
 
     def test_create_order_succesful(self):
         order_item = OrderItem.objects.create(
             order=self.order,
-            product=self.product,
-            quantity=3,
-            price=self.product.precio
+            producto=self.product,
+            cantidad=3,
+            precio=self.product.precio
         )
 
         self.assertEqual(self.order.items.count(), 1) 
-        self.assertEqual(order_item.quantity, 3)
-        self.assertEqual(order_item.price, self.product.precio)
+        self.assertEqual(order_item.cantidad, 3)
+        self.assertEqual(order_item.precio, self.product.precio)
