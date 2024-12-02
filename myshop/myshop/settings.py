93520cbefa1350e14b18174c5fa3bd764e1a06cd
Daniel Flores <danflode@alum.us.es>
2024-12-02 19:37:20 +0100

    fix(static_root): fix static

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -117,8 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_ROOT = 'static_root/'
+STATIC_URL = "/static/"
+
+STATIC_ROOT = '/home/daniflo/static/static_root'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
